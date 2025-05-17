65dd8aca42c46db7e84fb223fdd46daa243472cd
AlexPaul <bookingalexpaul@gmail.com>
2025-05-17 01:10:55 -0500

    style: restore global CSS import in layout for app styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import { PlayerProvider } from "./context/PlayerContext"
+import "./globals.css"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
