6fb7e85b4928a7d938eb167c709fd96ce968e916
AlexPaul <bookingalexpaul@gmail.com>
2025-05-17 01:40:30 -0500

    test: fuerza texto rojo grande en /thelab para debug visual

--- app/thelab/page.tsx
+++ app/thelab/page.tsx
@@ -3,6 +3,6 @@ export const dynamic = "force-dynamic";
 import React from 'react';
 
 export default function TheLab() {
-  return <div>Test</div>;
+  return <div style={{ color: 'red', fontSize: 40, zIndex: 9999, position: 'relative' }}>TEST</div>;
 }
 // TODO: Descomentar bloques poco a poco para aislar el error SSR
