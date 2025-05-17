65daea6ea89f61509f23d84532a3d2f9058c7662
AlexPaul <bookingalexpaul@gmail.com>
2025-05-17 12:59:32 -0500

    test: Agregar página de prueba para aislar problemas de renderizado

---
+++ app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+  return (
+    <div style={{ color: 'white', fontSize: 32, textAlign: 'center', marginTop: 100 }}>
+      Página de prueba funcionando
+    </div>
+  );
+} 
