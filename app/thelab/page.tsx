721a4bc546f8979c1202fca1af2e87369b7205c2
AlexPaul <bookingalexpaul@gmail.com>
2025-05-17 12:54:12 -0500

    fix: Renderizar correctamente TheLabContent en /thelab sin dynamic ni Suspense

--- app/thelab/page.tsx
+++ app/thelab/page.tsx
@@ -1,16 +1,6 @@
 "use client";
-import dynamic from "next/dynamic";
-import React from 'react';
+import TheLabContent from "../../components/TheLabContent";
 
-const TheLabContent = dynamic(() => import("../../components/TheLabContent"), { 
-  ssr: false,
-  loading: () => <div>Loading...</div>
-});
-
-export default function TheLab() {
-  return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-      <TheLabContent />
-    </React.Suspense>
-  );
+export default function TheLabPage() {
+  return <TheLabContent />;
 } 
