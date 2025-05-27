676f431546aa2180d88f9fd8da01359589b099d9
AlexPaul <bookingalexpaul@gmail.com>
2025-05-27 12:09:58 -0500

    fix: corrige imports relativos en carousel.tsx para compatibilidad cross-platform

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -7,8 +7,8 @@ import useEmblaCarousel, {
 import { ArrowLeft, ArrowRight } from "lucide-react"
 import Autoplay from 'embla-carousel-autoplay'
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "../../lib/utils"
+import { Button } from "./button"
 
 type CarouselApi = UseEmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
